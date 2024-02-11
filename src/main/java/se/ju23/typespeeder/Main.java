320e9d3a31391c59fddbaf98c8cb7a063f218c72
EmSley77 <emanuelsleyman@gmail.com>
2024-02-11 10:54:15 +0100

    fixed Main so its works with other classes

--- src/main/java/se/ju23/typespeeder/Main.java
+++ src/main/java/se/ju23/typespeeder/Main.java
@@ -3,6 +3,11 @@ package se.ju23.typespeeder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import se.ju23.typespeeder.databas.Leaderboard;
+import se.ju23.typespeeder.databas.User;
+import se.ju23.typespeeder.databas.iUser;
+import se.ju23.typespeeder.logic.AttemptRepo;
+import se.ju23.typespeeder.logic.iGameTask;
 
 import java.util.Optional;
 import java.util.Scanner;
@@ -12,44 +17,22 @@ public class Main  implements CommandLineRunner {
     @Autowired
     static AttemptRepo attemptRepo;
     @Autowired
-    static iUser iuser;
+    public static iUser iuser;
     @Autowired
     static iGameTask igametask;
     @Autowired
     static Leaderboard leaderboard;
+    @Autowired
+    public Main(iUser iuser){
+        this.iuser = iuser;
+    }
+    private User user;
+
+    public static Scanner input = new Scanner(System.in);
 
     @Override
     public void run(String[] args)  {
-        System.out.println();
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Välkommen till TypeSpeeder!");
-        System.out.println("1. Logga in");
-        System.out.println("2. Skapa användare");
-        System.out.println("3. Exit");
-
-        System.out.println("Skriv in ditt alternativ: ");
-        System.out.print(">");
-        String choice = input.nextLine();
-
-        switch(choice) {
-            case "1":
-                System.out.println("Du valde att logga in.");
-
-                // Add login functionality here
-
-                break;
-            case "2":
-                System.out.println("Du valde att skapa en användare.");
-                // Add create user functionality here
-                break;
-            case "3":
-                System.out.println("Du valde att avsluta. Hejdå!");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Ogiltigt svar. Testa igen");
-        }
     }
 
 }
